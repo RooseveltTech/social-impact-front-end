effc294af32260d03f8ce009c4d38ef6ac855d1c
RooseveltTech <rooseveltabandy@gmail.com>
2023-08-17 17:10:23 +0100

    frontend

--- green-air/src/app/blogs/page.tsx
+++ green-air/src/app/blogs/page.tsx
@@ -21,7 +21,7 @@ type Blog = {
 
 export default async function Blogs() {
     const blogs = await fetch(
-        'http://backendgreenair.azurewebsites.net/air/v1/blog/', {
+        'https://backendgreenair.azurewebsites.net/air/v1/blog/', {
         "headers": {
             "content-type": 'application/json',
         },
